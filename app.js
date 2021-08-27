6fd0cba82bac00c650b2888ac7b0e78fc33bf6fe
Rexequeil Guerrero <73897956+rexguerz@users.noreply.github.com>
2021-08-28 04:58:53 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'Jimmy',
+            lastName: 'Doe',
+            email: 'jimmydoe@email.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+    },
+
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+
+            this.firstName =  results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app')
